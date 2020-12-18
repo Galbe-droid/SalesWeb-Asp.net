9dc4ebc163ba0a09a2b19459b1bc62c545f035aa
Gabriel Lima Bertoldo <gabriel-bertoldo@hotmail.com>
2020-12-18 03:35:42 -0300

    other entities and second migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,5 +9,24 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Sellers> Sellers { get; set; }
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Sellers sellers)
+        {
+            Sellers.Add(sellers);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMVC/Models/Enum/SalesStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models.Enum
+{
+    public enum SalesStatus : int
+    {
+        PENDING = 0,
+        BILLED = 1,
+        CANCELED = 2
+    }
+}
